a129e490f745145ee0699b14f612712b0f272eec
shweta-huulke <s.kaul@huulke.com>
2025-04-19 02:33:05 +0530

    updaye

--- routes/user.js
+++
@@ -1,6 +0,0 @@
-const userController = require('../controllers/UserController');
-
-module.exports = (app) => {
-  app.get('/api/users', userController.getAllUsers);
-  app.post('/api/users', userController.createUser);
-};
